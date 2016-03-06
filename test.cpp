8f5f4b80bad6582eb858b561814a642b5ce98a60
李咸海 <lixianhai@lixianhaideMacBook-Pro.local>
2016-03-06 20:09:00 +0800

    this is test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日  3/ 6 20:08:02 2016
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"smartlxh you are the best";
+}
